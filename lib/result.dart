7b2a97d74ac7dc8590cdcb92797dedd3ec22e19d
PistonShot1 <nandaprian7@gmail.com>
2023-04-10 18:07:39 +0800

    first commit

---
+++ lib/result.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resultText = 'You did it!';
+    if (resultScore <= 8) {
+      resultText = 'We\'re quite different';
+    } else if (resultScore <= 12) {
+      resultText = 'We have some similaraties';
+    } else if (resultScore <= 16) {
+      resultText = 'We are pretty similar!';
+    } else {
+      resultText = 'Yo boi!';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+          resultPhrase,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        TextButton(
+          style: TextButton.styleFrom(primary: Colors.blue),
+          child: Text('Restart Quiz'),
+          onPressed: resetHandler,
+        ),
+      ],
+    );
+  }
+}
